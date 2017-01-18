e2594903e172db2168f50004c3bd53b51f71ff35
rsgc-bagga-p <pbagga@rsgc.on.ca>
2017-01-18 12:23:43 -0500

    Added a dice class and now the program takes the input for the game board and dice sides. Now to create the algorithm to play the game.

--- Dice Game No.2/Dice Game No.2/main.swift
+++ Dice Game No.2/Dice Game No.2/main.swift
@@ -8,25 +8,63 @@
 
 import Foundation
 
-var gameBoardInput : String = ""
+var gameBoardInput : String = "" //user input variable
+var gameBoardArray = [String]()  //game board values
+var diceSides: Int = 0
 
 repeat {
     
     print("Please enter your game board values with spaces between them.", terminator:"\n")
     
-    if let input = readLine(strippingNewline: true){
+    if let input = readLine(strippingNewline: true){ // read the command line
         
-        if input != "" {
+        if input != "" { // if there is anything to be read then...
             
-            gameBoardInput = input
+            gameBoardInput = input //place input into variable
+            
+            //seperates input into array
+            gameBoardArray = gameBoardInput.components(separatedBy: " ")
             
         } else {
             
-            print("please enter values")
+            print("please enter values") //error line
             
         }
 }
+ 
+    repeat {
+        
+        print("Enter your preferred dice sides.", terminator: "\n")
+        
+        if let input2 = readLine(strippingNewline: true) {
+            
+            if let input2AsInteger = Int(input2) {
+                
+                diceSides = input2AsInteger
+                
+            } else {
+                
+                print("Please enter your preferred dice sides.")
+                
+            }
+            
+        }
+        
+        
+    } while diceSides == 0
     
-} while gameBoardInput == "" && gameBoardInput.characters.count != 9
+    
+} while gameBoardInput == "" && gameBoardArray.count == 5 // if there is anything inputed and the gameBoard array has all five values
 
-let gameBoardArray = gameBoardInput.components(separatedBy: " ")
+print(gameBoardArray) //check
+
+
+
+class Dice {
+    var sides: Int
+    init(sides: Int){
+        self.sides = sides
+    }
+}
+
+
